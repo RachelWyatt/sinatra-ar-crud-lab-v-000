22b02e75fd2c69e715b61b02c7611df0109eb9f5
Rachel Wyatt <rw126@evansville.edu>
2019-01-12 00:56:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{@article.id}"
   end
 
-  delete 'articles/:id' do
+  delete 'articles/2' do
     @article = Article.find_by_id(params[:id])
     binding.pry
     @article.delete
